33528e50bf6a2b5747285e2f2cba529658b807f7
Andreé <andree.serranourraca@gmail.com>
2023-11-09 19:06:51 +0100

    Rediseño la carta pequeña de los pokemon

--- lib/widgets/SmallCard/small_card.dart
+++ lib/widgets/SmallCard/small_card.dart
@@ -17,22 +17,39 @@ class SmallCard extends StatelessWidget{
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
 
-    return Card(
-      color: Colors.lightGreen,
-      child: InkWell(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ImageSvg(name: pokemonCard.name, width: 70, height: 70,),
-            SizedBox(height: 10,),
-            Text(pokemonCard.name.capitalize())
-          ],
-        ),
-        onTap: () {
+    return GestureDetector(
+      onTap:(){
          appState.selectedPokemon();
          appState.selectedPokemonName(pokemonCard.name);
-        },
-      ),
+      } ,
+      child: Container(
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[200],
+          borderRadius: BorderRadius.all(Radius.circular(20))
+        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children:[
+                  Positioned.fill(
+                    child: Image.network("https://cdn.imgbin.com/13/20/18/imgbin-pok-ball-pok-mon-go-computer-icons-pokemon-go-ypKEEbx02zhFCsMwmbGN6vzxt.jpg")
+                  ),
+                  ImageSvg(name: pokemonCard.name, width: 90, height: 90,)
+                ] 
+              ),
+              SizedBox(height: 10,),
+              Text(pokemonCard.name.capitalize(),
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white70
+                ),
+              )
+            ],
+          ),
+      )
     );
   }
 }
